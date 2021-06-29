00f9029b49ce0b3c4473c9e301cf120c6b19b212
yasmin <yasmintolba227@gmail.com>
2021-06-29 23:18:03 +0200

    api

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <input type="text" placeholder="search" id="input" />
+      <button id="bt" v-on:click="fetch()">search</button>
     </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+  methods: {
+    fetch() {
+      var field = document.querySelector("input[id=input]").value;
+      fetch(
+        `https://www.mapquestapi.com/geocoding/v1/address?key=sSHcyXqUGQGnKJav2smr5rcK8MAeAHOO&location=${field}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          let location = `${data.results[0].locations[0].latLng.lat},
+            ${data.results[0].locations[0].latLng.lng}`;
+          fetch(
+            `https://data.climacell.co/v4/timelines?apikey=EcGPm2HkA6OceXep7AF78N3Eiaa5s0Fm&location=${location}&fields=temperature,windSpeed&timesteps=1d`
+          )
+            .then((innerdata) => innerdata.json())
+            .then((result) => console.log(result));
+        });
+    },
+  },
+};
+</script>
